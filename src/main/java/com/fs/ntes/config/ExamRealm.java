9b827936376e04b4252f38aabcdf974a33cfc726
fengsong <hardly.a.chanllenge.for.me@gmail.com>
2017-09-28 15:07:21 +0800

    model提交

---
+++ src/main/java/com/fs/ntes/config/ExamRealm.java
@@ -0,0 +1,33 @@
+package com.fs.ntes.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.util.ByteSource;
+
+public class ExamRealm  {
+   /* @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
+            throws AuthenticationException {
+        System.out.println("MyShiroRealm.doGetAuthenticationInfo()");
+        //获取用户的输入的账号.
+        String username = (String) token.getPrincipal();
+        System.out.println(token.getCredentials());
+        //通过username从数据库中查找 User对象，如果找到，没找到.
+        //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+        UserInfo userInfo = userInfoService.findByUsername(username);
+        System.out.println("----->>userInfo=" + userInfo);
+        if (userInfo == null) {
+            return null;
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                userInfo, //用户名
+                userInfo.getPassword(), //密码
+                ByteSource.Util.bytes(userInfo.getCredentialsSalt()),//salt=username+salt
+                getName()  //realm name
+        );
+        return authenticationInfo;
+    }*/
+}
