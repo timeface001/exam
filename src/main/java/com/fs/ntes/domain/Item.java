a098be4daa3d92e0c5e3f366c52084023d8ff0d8
fengsong <hardly.a.chanllenge.for.me@gmail.com>
2017-12-04 17:54:44 +0800

    题库开发

---
+++ src/main/java/com/fs/ntes/domain/Item.java
@@ -0,0 +1,85 @@
+package com.fs.ntes.domain;
+
+import java.util.Date;
+
+public class Item {
+    private Integer id;
+
+    private String name;
+
+    private Integer year;
+
+    private String desc;
+
+    private String cover;
+
+    private Integer uid;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc == null ? null : desc.trim();
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover == null ? null : cover.trim();
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/fs/ntes/domain/mapper/ItemMapper.java
@@ -0,0 +1,17 @@
+package com.fs.ntes.domain.mapper;
+
+import com.fs.ntes.domain.Item;
+
+public interface ItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Item record);
+
+    int insertSelective(Item record);
+
+    Item selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Item record);
+
+    int updateByPrimaryKey(Item record);
+}
